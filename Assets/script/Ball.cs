cd5159c7e6bdc88babf41254f455ddb3890abc13
sky <865701546@qq.com>
2016-04-25 23:21:05 +0800

    f

---
+++ Assets/script/Ball.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+    private Rigidbody rd;
+    public int force = 5;
+	// Use this for initialization
+	void Start () {
+        rd = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rd.AddForce(new Vector3(h,0,v) * force);
+	}
+}
---
+++ Assets/script/FollowTarget.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTarget : MonoBehaviour {
+    public Transform ball;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = ball.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = ball.position - offset;
+	}
+}
